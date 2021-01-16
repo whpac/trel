afcc9d6285116724d1de6b5738307429a58bc63b
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-01-16 19:42:27 +0100

    Added equality check

--- src/traffic/agency/vehicle.ts
+++ src/traffic/agency/vehicle.ts
@@ -7,4 +7,13 @@ export default class Vehicle {
         public readonly nextStopSeq: number,
         public readonly time: bigint
     ) { }
+
+    public equals(v: Vehicle) {
+        if(this.id != v.id) return false;
+        if(this.tripId != v.tripId) return false;
+        if(this.routeId != v.routeId) return false;
+        if(this.nextStopSeq != v.nextStopSeq) return false;
+
+        return true;
+    }
 }
