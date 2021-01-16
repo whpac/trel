5c00043dff405007004feb88ad02c24202619373
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-01-16 14:48:14 +0100

    Make casing consistent on entities

--- src/parsers/feed_entities/trip_descriptor.ts
+++ src/parsers/feed_entities/trip_descriptor.ts
@@ -2,12 +2,12 @@ import BaseMessage from '../protobuf/base_message';
 import { Message } from '../protobuf/message';
 
 export default class TripDescriptor extends BaseMessage {
-    public TripId: string = "";      // 1
-    public StartTime: string = "";   // 2
-    public StartDate: string = "";   // 3
-    public ScheduleRelationship: EnumScheduleRelationship = EnumScheduleRelationship.SCHEDULED;  // 4
-    public RouteId: string = "";     // 5
-    public DirectionId: number = 0;    // 6
+    public tripId: string = "";      // 1
+    public startTime: string = "";   // 2
+    public startDate: string = "";   // 3
+    public scheduleRelationship: EnumScheduleRelationship = EnumScheduleRelationship.SCHEDULED;  // 4
+    public routeId: string = "";     // 5
+    public directionId: number = 0;    // 6
 
     public getEmbeddedMessage(field_number: number): Message {
         throw new Error(`Field number ${field_number} is not message.`);
@@ -20,16 +20,16 @@ export default class TripDescriptor extends BaseMessage {
     public setField(field_number: number, bytes: Buffer | number | bigint): void {
         if(typeof bytes === 'number') {
             switch(field_number) {
-                case 4: this.ScheduleRelationship = bytes; break;
-                case 6: this.DirectionId = bytes; break;
+                case 4: this.scheduleRelationship = bytes; break;
+                case 6: this.directionId = bytes; break;
             }
         }
         if(bytes instanceof Buffer) {
             switch(field_number) {
-                case 1: this.TripId = bytes.toString('utf-8'); break;
-                case 2: this.StartTime = bytes.toString('utf-8'); break;
-                case 3: this.StartDate = bytes.toString('utf-8'); break;
-                case 5: this.RouteId = bytes.toString('utf-8'); break;
+                case 1: this.tripId = bytes.toString('utf-8'); break;
+                case 2: this.startTime = bytes.toString('utf-8'); break;
+                case 3: this.startDate = bytes.toString('utf-8'); break;
+                case 5: this.routeId = bytes.toString('utf-8'); break;
             }
         }
     }
