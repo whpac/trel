29e7d7c285246dc7ef6b21dd3301d373b514a87a
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-01-18 13:12:43 +0100

    Added a class with the main logic

---
+++ src/gtfs.ts
@@ -0,0 +1,55 @@
+import DirectoryLister from './loaders/directory_lister';
+import FileLoader from './loaders/file_loader';
+import FeedMessage from './parsers/feed_entities/feed_message';
+import FeedParser from './parsers/gtfs-rt/feed_parser';
+import PbReader from './parsers/protobuf/pb_reader';
+import StopSchedule from './traffic/agency/stop_schedule';
+import DifferentialAnalyzer from './traffic/realtime/differential_analyzer';
+import StopEventMemoryStorage from './traffic/realtime/stop_event_memory_storage';
+import StopEventStorage from './traffic/realtime/stop_event_storage';
+import StopTimesRegistry from './traffic/realtime/stop_times_registry';
+
+export default class Gtfs {
+    protected analyzer: DifferentialAnalyzer;
+    protected stopTimesRegistry: StopTimesRegistry | undefined;
+    protected stopStorage: StopEventStorage;
+
+    public constructor() {
+        this.analyzer = new DifferentialAnalyzer();
+        this.stopStorage = new StopEventMemoryStorage();
+    }
+
+    public loadStops(schedule: StopSchedule): void {
+        this.stopTimesRegistry = new StopTimesRegistry(schedule, this.stopStorage);
+    }
+
+    public setStopStorage(storage: StopEventStorage): void {
+        this.stopStorage = storage;
+        this.stopTimesRegistry?.setStorage(this.stopStorage);
+    }
+
+    public async readFile(path: string): Promise<void> {
+        if(this.stopTimesRegistry === undefined) throw new Error("StopSchedule hasn't been loaded.");
+
+        let loader = new FileLoader();
+        let data_stream = loader.load(path);
+
+        let fm = new FeedMessage();
+        await PbReader.parseMessageInto(data_stream, fm);
+        let vehicles = FeedParser.parseFeed(fm);
+
+        this.analyzer.analyzeNewData(vehicles, this.stopTimesRegistry);
+
+        console.log('New GTFS-RT data was read');
+    }
+
+    public async readDirectory(path: string): Promise<void> {
+        let files = await DirectoryLister.listDir(path, true);
+        files = files.sort();
+
+        for(let file of files) {
+            if(!file.endsWith(".pb")) continue;
+            await this.readFile(file);
+        }
+    }
+}
